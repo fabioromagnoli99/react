3bc904c48439bc7ac780ce9625764e2834ddd449
Fabio Romagnoli <32691245+fabioromagnoli99@users.noreply.github.com>
2022-01-29 13:12:21 +0000

    Example useState

---
+++ example/index.jsx
@@ -0,0 +1,25 @@
+// example when  we use one button increment and decrement
+
+import {useState} from 'react'
+
+function App () {
+    const [counter, setCounter] = useState(0)
+
+    function decrement () {
+        setCounter(counter -1)        
+    }
+
+    function increment () {
+        setCounter(counter +1)
+    }
+
+   return (
+        <>
+        <h1>Contador: {counter}</h1>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </>
+    ) 
+}
+
+export default App
