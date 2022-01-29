4ca8b5fc2cfb94f70e5ff24a19fc35a98700f72f
Fabio <fabio.gromagnoli@gmail.com>
2022-01-29 10:51:41 +0000

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+
+
+function App () {
+    return (
+    <div className="container">
+        <header className="header">Header</header>
+        <nav className="menu">Menu</nav>
+        <p className="sidebar">Sidebar</p>
+        <p className="conteudo">Conteudo</p>
+        <footer className="footer">Footer</footer>
+    </div>
+    )
+}
+
+
+export default App
